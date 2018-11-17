08ef71673eabfa43dfe52b149302171c8ab514f7
Ame_Wulei <ame_wulei@hotmail.com>
2018-11-17 21:13:01 +0800

    更新

---
+++ PythonApp/DjangoWeb/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\我的项目\Quantitative_Investment\PythonApp\DjangoWeb\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
