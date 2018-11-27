8174b02389a99019a30c72b942738f3e1b82a553
Ame_Wulei <ame_wulei@hotmail.com>
2018-11-27 15:36:51 +0800

    更新数据

--- PythonApp/PythonApp/module3.py
+++ PythonApp/PythonApp/module3.py
@@ -1,10 +1,15 @@
 import sys
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from New import class1 as aa
+from WinFromApp import MainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow
+Ui_MainWindow = MainWindow.Ui_MainWindow
+class MyWindow(QMainWindow,Ui_MainWindow):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+
 if __name__ == '__main__':
-    app=QApplication(sys.argv)
-    table=aa.Table()
-    table.show()
+    app = QApplication(sys.argv)
+    myWin = MyWindow()
+    myWin.show()
     sys.exit(app.exec_())
